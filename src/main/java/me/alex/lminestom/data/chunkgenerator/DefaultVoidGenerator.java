2b44be59c20d143dbfcc8fd7bf205b45625fcdfb
GoldenGamerLP <65727384+GoldenGamerLP@users.noreply.github.com>
2022-01-02 21:04:23 +0100

    Testing new Chunk Generator

--- src/main/java/me/alex/lminestom/data/chunkgenerator/DefaultVoidGenerator.java
+++ src/main/java/me/alex/lminestom/data/chunkgenerator/DefaultVoidGenerator.java
@@ -9,22 +9,24 @@ import net.minestom.server.instance.block.Block;
 import net.minestom.server.world.biomes.Biome;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class DefaultVoidGenerator implements ChunkGenerator {
 
-    private final LMinestomNoise overworldNoise;
-    private final LMinestomNoise landmassNoise;
+    private final LMinestomNoise overN;
+    private final LMinestomNoise lNoise;
     private final LMinestomNoise riverTerrainNoise;
     private final Random Seed;
 
     public DefaultVoidGenerator() {
         this.Seed = new Random(22112005);
-        this.overworldNoise = new LMinestomNoise(LMinestomNoiseType.OverworldTerrain,Seed.nextInt());
-        this.landmassNoise = new LMinestomNoise(LMinestomNoiseType.LandmassTerrain,Seed.nextInt());
+        this.overN = new LMinestomNoise(LMinestomNoiseType.OverworldTerrain,Seed.nextInt());
+        this.lNoise = new LMinestomNoise(LMinestomNoiseType.LandmassTerrain,Seed.nextInt());
         this.riverTerrainNoise = new LMinestomNoise(LMinestomNoiseType.RiverTerrain,Seed.nextInt());
     }
 
@@ -35,28 +37,36 @@ public class DefaultVoidGenerator implements ChunkGenerator {
                 int posX = chunkX*16+x;
                 int posZ = chunkZ*16+z;
 
+                var e = (1.00 * overN.GetNoise(1 * posX, 1 * posZ)
+                        + 0.50 * overN.GetNoise( 2 * posX,  2 * posZ)
+                        + 0.25 * overN.GetNoise( 4 * posX,  4 * posZ)
+                        + 0.13 * overN.GetNoise( 8 * posX,  8 * posZ)
+                        + 0.06 * overN.GetNoise(16 * posX, 16 * posZ)
+                        + 0.03 * overN.GetNoise(32 * posX, 32 * posZ));
+                e = e / (1.00 + 0.50 + 0.25 + 0.13 + 0.06 + 0.03);
+                e = Math.pow(e, 2.85);
+                //e = (1 + e - (Math.sqrt(posX * posX + posZ * posZ) / Math.sqrt(0.5))) / 2;
+                //e = (1 + e - (Math.abs(posX)));
+                //e = Math.round(e * 32) / 32.0;
+                //e = (1 + e - (Math.max(Math.abs(posX),Math.abs(posZ)))) / 2;
 
+                var m = (1.00 * lNoise.GetNoise( 1 * posX,  1 * posZ)
+                        + 0.75 * lNoise.GetNoise( 2 * posX,  2 * posZ)
+                        + 0.33 * lNoise.GetNoise( 4 * posX,  4 * posZ)
+                        + 0.33 * lNoise.GetNoise( 8 * posX,  8 * posZ)
+                        + 0.33 * lNoise.GetNoise(16 * posX, 16 * posZ)
+                        + 0.50 * lNoise.GetNoise(32 * posX, 32 * posZ));
+                m = m / (1.00 + 0.75 + 0.33 + 0.33 + 0.33 + 0.50);
+
+                var height = (int) (e * 16) + 64;
+                Block block = getBlock(e,m);
+                for(int y = 1; y < height; y++) {
+                    batch.setBlock(x,y,z,Block.STONE);
+                }
+                batch.setBlock(x,height,z,block);
                 batch.setBlock(x, 0, z, Block.BEDROCK);
 
-                // Get normalised balance between water and land
-                double landmassDelta = landmassNoise.GetNoise(posX, posZ);
-                double waterDelta = riverTerrainNoise.GetNoise(posX, posZ);
-                double ratio = 1 / (landmassDelta + waterDelta);
-                landmassDelta = landmassDelta * ratio;
-                waterDelta = waterDelta * ratio;
 
-                // Calculate height based on normalised balance
-                double terrainHeight = 32 + 64 * landmassDelta * Math.pow(overworldNoise.GetNoise(posX, posZ), 1/3.0);
-                double waterHeight = 64 * waterDelta * overworldNoise.GetNoise(posX, posZ);
-                int height = (int) (terrainHeight + waterHeight);
-
-                for (int y = 1; y < height; y++) {
-                    batch.setBlock(posX, y, posZ, Block.DIRT);
-                }
-                batch.setBlock(posX, height, posZ, Block.DIRT);
-                for (int y = height; y < 64; y++) {
-                    batch.setBlock(posX, y, posZ, Block.WATER);
-                }
             }
         }
     }
@@ -69,5 +79,35 @@ public class DefaultVoidGenerator implements ChunkGenerator {
     @Override
     public @Nullable List<ChunkPopulator> getPopulators() {
         return null;
+    }
+
+    private Block getBlock(double e, double m) {
+        if (e < 0.1) return Block.WATER;
+        if (e < 0.12) return Block.SAND;
+
+        if (e > 0.8) {
+            if (m < 0.1) return Block.COARSE_DIRT;
+            if (m < 0.2) return Block.SNOW;
+            if (m < 0.5) return Block.POWDER_SNOW;
+            return Block.SNOW_BLOCK;
+        }
+
+        if (e > 0.6) {
+            if (m < 0.33) return Block.SAND;
+            if (m < 0.66) return Block.SANDSTONE;
+            return Block.SANDSTONE;
+        }
+
+        if (e > 0.3) {
+            if (m < 0.16) return Block.GRASS;
+            if (m < 0.50) return Block.GRASS_BLOCK;
+            if (m < 0.83) return Block.GRASS_BLOCK;
+            return Block.JUNGLE_WOOD;
+        }
+
+        if (m < 0.16) return Block.SAND;
+        if (m < 0.33) return Block.GRASS_BLOCK;
+        if (m < 0.66) return Block.GRASS_BLOCK;
+        return Block.JUNGLE_LEAVES;
     }
 }
--- src/main/java/me/alex/lminestom/data/chunkgenerator/LMinestomNoise.java
+++ src/main/java/me/alex/lminestom/data/chunkgenerator/LMinestomNoise.java
@@ -1,5 +1,6 @@
 package me.alex.lminestom.data.chunkgenerator;
 
+
 import de.articdive.jnoise.JNoise;
 
 public class LMinestomNoise {
@@ -13,22 +14,22 @@ public class LMinestomNoise {
                     jNoise = JNoise.newBuilder().superSimplex().setSeed(random).setFrequency(0.0025).build();
             case 1 ->
                     //Overworld Terrain
-                    jNoise = JNoise.newBuilder().superSimplex().setSeed(random).setFrequency(0.01).build();
+                    jNoise = JNoise.newBuilder().superSimplex().setSeed(random).setFrequency(0.015).build();
             case 2 ->
                     //OverworldFauna
-                    jNoise = JNoise.newBuilder().superSimplex().setSeed(random).setFrequency(0.01).build();
-            case 3 -> jNoise = JNoise.newBuilder().superSimplex().setSeed(random).setFrequency(0.01).build();
-            case 4 -> jNoise = JNoise.newBuilder().superSimplex().setSeed(random).setFrequency(0.01).build();
-            case 5 -> jNoise = JNoise.newBuilder().superSimplex().setSeed(random).setFrequency(0.01).build();
+                    jNoise = JNoise.newBuilder().superSimplex().setSeed(random).setFrequency(0.015).build();
+            case 3 -> jNoise = JNoise.newBuilder().superSimplex().setSeed(random).setFrequency(0.015).build();
+            case 4 -> jNoise = JNoise.newBuilder().superSimplex().setSeed(random).setFrequency(0.015).build();
+            case 5 -> jNoise = JNoise.newBuilder().superSimplex().setSeed(random).setFrequency(0.015).build();
             default -> jNoise = JNoise.newBuilder().superSimplex().setSeed(random).setFrequency(0.01).build();
         }
     }
 
     public double GetNoise(double x, double y) {
-        return (jNoise.getNoise(x, y) + 1) / 2;
+        return (jNoise.getNoise(x, y)) / 2 + 0.5;
     }
 
     public double GetNoise(double x, double y, double z) {
-        return (jNoise.getNoise(x, y, z) + 1) / 2;
+        return (jNoise.getNoise(x, y, z)) / 2 + 0.5;
     }
 }
